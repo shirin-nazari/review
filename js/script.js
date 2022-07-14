f8241a8ee06b4f5851ef7957de1950289ccfe47d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-07-14 23:08:14 +0430

    rest

--- js/script.js
+++ js/script.js
@@ -46,3 +46,32 @@ function maxMin(arr1, arr2) {
     Math.min(...arr.map((i) => Math.abs(i))),
   ];
 }
+// function sum() {
+//   console.log(arguments);
+// }
+// sum();
+
+function sum(...nums) {
+  console.log(nums);
+  return console.log(nums.reduce((acc, curr) => acc + curr));
+}
+
+// sum(1, 2, 3, 4, 5, 6, 100);
+
+//$ ORDER MATTERS:
+
+// function raceResults(gold, silver, ...others) {
+//   console.log(`gold medal goes to: ${gold}`);
+//   console.log(`silver medal goes to: ${silver}`);
+//   console.log(`Thanks to: ${others}`);
+// }
+// raceResults('Schumacher');
+// raceResults('Schumacher', 'Hamilton');
+// raceResults('Schumacher', 'Hamilton', 'Vette', 'Roseberg', 'Button');
+
+function raceResults2(...others) {
+  console.log(`gold medal goes to: ${others[0]}`);
+  console.log(`silver medal goes to: ${others[1]}`);
+  console.log(`Thanks to: ${others}`);
+}
+raceResults2('Schumacher', 'Hamilton', 'Vette', 'Roseberg', 'Button');
